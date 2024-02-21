f79cbfe09a94ce987cbb5a37ca408cd49887bcac
Isaac Solomon <G00424127@atu.ie>
2024-02-21 11:21:02 +0000

    lab3

---
+++ src/app/a/a.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-a',
+  standalone: true,
+  imports: [],
+  templateUrl: './a.component.html',
+  styleUrl: './a.component.css'
+})
+export class AComponent {
+
+}
